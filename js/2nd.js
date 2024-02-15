9d51a7c665f1b5752752eb0e70e274f6102c479b
roksana35 <Roksanaakter7235@gmail.com>
2024-02-15 17:33:32 +0600

    add some code into js file

--- js/2nd.js
+++ js/2nd.js
@@ -24,3 +24,22 @@ function setbrColor (elementId){
     const setbg = document.getElementById(elementId);
     setbg.classList.add('bg-orange-400');
 }
+function removebrColor (elementId){
+    const setbg = document.getElementById(elementId);
+    setbg.classList.remove('bg-orange-400');
+}
+function setTextElementById (elementId,value){
+    const element = document.getElementById (elementId);
+    element.innerText = value;
+}
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+function getelementText (elementId){
+    const element = document.getElementById(elementId);
+     const text = element.innerText;
+     return text;
+}
--- js/alpha.js
+++ js/alpha.js
@@ -9,6 +9,49 @@
 //     playgroun.classList.remove('hidden');
     
 // }
+function habdleKeyboardPress (event){
+    const keypress = event.key;
+    // console.log( 'player press',keypress);
+    // if player press Esc button then game over
+    if(keypress==='Escape'){
+        gameover();
+    }
+
+
+    const currentalphabatElement = document.getElementById('carent-alphamet');
+    const currentAlphabat = currentalphabatElement.innerText;
+    const expectedAlphabet = currentAlphabat.toLowerCase();
+    console.log('expected',expectedAlphabet);
+    if(keypress===expectedAlphabet){
+        console.log('you got point');
+        removebrColor(expectedAlphabet);
+        // score
+        const currentscoreElement = document.getElementById('score');
+        const currentscore = currentscoreElement.innerText;
+        const currentS = parseInt(currentscore);
+        const finalscore = currentS +1;
+        currentscoreElement.innerText= finalscore;
+        // console.log(finalscore);
+        continueGame();
+    }
+    else{
+        console.log('you dont get any poin');
+        const currentLifeElement = document.getElementById('life');
+        const currentlifetext = currentLifeElement.innerText;
+        const currentlife = parseInt(currentlifetext);
+        const newlife = currentlife-1;
+        currentLifeElement.innerText = newlife;
+        if(newlife===0){
+            gameover();
+
+        }
+
+    }
+}
+document.addEventListener('keyup', habdleKeyboardPress);
+
+
+
 function continueGame (){
     const alphabet =  getARandomAlphabet();
     console.log(alphabet);
@@ -20,7 +63,24 @@ function continueGame (){
 
 function play (){
     hideelementid('home');
+    hideelementid('final-score');
     setelementId('playground'); 
+    setTextElementById ('life',5);
+    setTextElementById('score',0);
     continueGame();
 
 }
+function gameover(){
+    hideelementid('playground');
+    setelementId('final-score');
+    // final-score
+    const lastScore= getTextElementValueById('score');
+    setTextElementById('point',lastScore);
+    const currentAlphabet =  getelementText('carent-alphamet');
+    removebrColor(currentAlphabet);
+
+}
+// function playAgain(){
+//     hideelementid('final-score');
+//     setelementId('playground');
+// }
